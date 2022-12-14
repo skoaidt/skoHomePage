113ac87a5f70a4e33be2855ba06f9badd5337e4c
조규용 <mvpcouple@naver.com>
2022-12-14 11:17:55 +0900

    v1.04 사업분야 수정

--- plugins/naver-map/map.js
+++ plugins/naver-map/map.js
@@ -1,13 +1,3 @@
-// var mapOptions = {
-//   center: new naver.maps.LatLng(37.536666445340444, 126.89517355935246),
-//   zoom: 18,
-// };
-// var marker = new naver.maps.Marker({
-//   position: new naver.maps.LatLng(37.3595704, 127.105399),
-//   map: map
-// });
-// var map = new naver.maps.Map('map', mapOptions);
-
 
 
 var HOME_PATH = window.HOME_PATH || '.';
@@ -25,9 +15,6 @@ var cityhall = new naver.maps.LatLng(37.53623023718519, 126.89503377521618),
 var contentString = [
         '<div class="text-center">',
         '   <h4>SK오앤에스 본사 </h4>',
-        // '   <p style="padding:20px;">서울특별시 영등포구 선유로 49길 23 아이에스BIZ타워 2차 9F (07208)<br />',
-        // '       <a href="http://www.skons.co.kr" target="_blank">http://www.skons.co.kr</a>',
-        // '   </p>',
         '</div>'
     ].join('');
 
